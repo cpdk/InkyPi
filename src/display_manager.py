53bf7b5037201c201b6b1de3e9d6a13eb0b152af
Christian Petersen <christian@ipeople.dk>
2025-03-22 07:08:20 +0100

    Local changes required to make it run

--- src/display_manager.py
+++ src/display_manager.py
@@ -8,12 +8,11 @@ class DisplayManager:
     def __init__(self, device_config):
         """Manages the display and rendering of images."""
         self.device_config = device_config
-        self.inky_display = auto()
+        self.inky_display = auto(ask_user = True, verbose = True)
         self.inky_display.set_border(self.inky_display.BLACK)
 
         # store display resolution in device config
-        if not device_config.get_config("resolution"):
-            device_config.update_value("resolution",[int(self.inky_display.width), int(self.inky_display.height)], write=True)
+        device_config.update_value("resolution",[int(self.inky_display.width), int(self.inky_display.height)], write=True)
 
     def display_image(self, image, image_settings=[]):
         """Displays the image provided, applying the image_settings."""
